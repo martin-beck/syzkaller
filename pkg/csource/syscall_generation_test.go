37517b4a9aec687fcf3fca18fa0a8a11fa2e399e
Martin Beck <martin.beck2@gmx.de>
2026-06-02 11:25:02 +0200

    Adds update to syscall generation test
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- pkg/csource/syscall_generation_test.go
+++ pkg/csource/syscall_generation_test.go
@@ -165,7 +165,7 @@ func testGenerationImpl(t *testing.T, test testData, target *prog.Target) (strin
 	}
 	var actualSyscalls []string
 	for _, execCall := range decoded.Calls {
-		actualSyscalls = append(actualSyscalls, ctx.fmtCallBody(execCall))
+		actualSyscalls = append(actualSyscalls, ctx.fmtCallBody(execCall, false))
 	}
 
 	if len(actualSyscalls) != len(test.calls) || len(actualSyscalls) != len(actualComments) {
