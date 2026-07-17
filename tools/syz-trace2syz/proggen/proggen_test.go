bbcb95accb48b18197b5ac0aae669dc927090e7d
Martin Beck <martin.beck2@gmx.de>
2026-07-17 13:56:37 +0200

    tools/syz-trace2syz: handle flattened string arrays
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- tools/syz-trace2syz/proggen/proggen_test.go
+++ tools/syz-trace2syz/proggen/proggen_test.go
@@ -24,6 +24,14 @@ func TestParse(t *testing.T) {
 		output string
 	}
 	tests := []Test{
+		// A single exec string-array element is flattened to BufferType by the parser.
+		// Keep this on a supported exec path so genArray must consume that buffer.
+		{`
+execve("\x2f\x62\x69\x6e\x2f\x74\x6f\x6f\x6c", ["\x2f\x62\x69\x6e\x2f\x74\x6f\x6f\x6c"], ["\x41\x3d\x42"]) = 0
+`, `
+syz_csb_execve()[0]
+`,
+		},
 		{`
 socket(37, 1, 0) = 3
 setsockopt(3, 278, 128, "abc", 3) = 0
