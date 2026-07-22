d1fb3cec35236194ff9de46c62f0ff06c1c4f57f
Martin Beck <martin.beck2@gmx.de>
2026-07-23 00:32:09 +0200

    sys/targets: declare AIO cancel eventfd dependency
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- sys/targets/targets.go
+++ sys/targets/targets.go
@@ -514,7 +514,7 @@ var oses = map[string]osCommon{
 			"syz_csb_io_pgetevents": {"io_setup", "io_pgetevents", "io_destroy"},
 			"syz_csb_io_destroy":    {"io_setup", "io_destroy"},
 			"syz_csb_io_submit":     {"io_setup", "io_submit", "io_getevents", "io_destroy"},
-			"syz_csb_io_cancel":     {"io_setup", "io_submit", "io_cancel", "io_getevents", "io_destroy"},
+			"syz_csb_io_cancel":     {"io_setup", "io_submit", "io_cancel", "io_getevents", "io_destroy", "eventfd2"},
 			"syz_clone3":            {"clone3", "exit"},
 			"syz_clone":             {"clone", "exit"},
 			"syz_pidfd_open":        {"pidfd_open"},
