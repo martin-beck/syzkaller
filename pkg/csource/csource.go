19d43bb3a5374bbf379919bfbce373231ec89753
Martin Beck <martin.beck2@gmx.de>
2026-08-03 14:38:59 +0200

    pkg/csource: preserve live dup3 destinations
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- pkg/csource/csource.go
+++ pkg/csource/csource.go
@@ -986,8 +986,10 @@ func (ctx *context) generateCalls(p prog.ExecProg, trace, addComments bool,
 		resCopyout := call.Index != prog.ExecNoCopyout
 		argCopyout := len(call.Copyout) != 0
 		isDup := ctx.opts.CSB && (call.Meta.CallName == "dup" || call.Meta.CallName == "dup3")
-		closeInitialDup := isDup && !resCopyout
-		closeRerunDup := isDup && call.Props.Rerun > 0
+		preserveDup3 := call.Meta.CallName == "dup3" &&
+			(resCopyout || execResultUsed(call.Args[1], p.Calls[ci+1:]))
+		closeInitialDup := isDup && !resCopyout && !preserveDup3
+		closeRerunDup := isDup && call.Props.Rerun > 0 && !preserveDup3
 		dupResultVar := fmt.Sprintf("csb_dup_res_%d", ci)
 		if closeInitialDup || closeRerunDup {
 			fmt.Fprintf(w, "\tintptr_t %s;\n", dupResultVar)
@@ -1292,6 +1294,26 @@ func fcntlCommand(call prog.ExecCall, command uint64) bool {
 	}
 	arg, ok := call.Args[1].(prog.ExecArgConst)
 	return ok && arg.Value == command
+}
+
+func execResultUsed(arg prog.ExecArg, calls []prog.ExecCall) bool {
+	result, ok := arg.(prog.ExecArgResult)
+	if !ok {
+		return false
+	}
+	for _, call := range calls {
+		for _, arg := range call.Args {
+			if other, ok := arg.(prog.ExecArgResult); ok && other.Index == result.Index {
+				return true
+			}
+		}
+		for _, copyin := range call.Copyin {
+			if other, ok := copyin.Arg.(prog.ExecArgResult); ok && other.Index == result.Index {
+				return true
+			}
+		}
+	}
+	return false
 }
 
 func localIOResources(p prog.ExecProg, target *prog.Target) map[uint64]bool {
