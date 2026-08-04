67b656c771831938d40e2dbccef4486000c756cd
Martin Beck <martin.beck2@gmx.de>
2026-08-04 11:06:48 +0200

    csource: share combined CSB regression helpers
    Signed-off-by: Martin Beck <martin.beck2@gmx.de>

--- pkg/csource/csb_io_uring_registered_test.go
+++ pkg/csource/csb_io_uring_registered_test.go
@@ -4,37 +4,8 @@
 package csource
 
 import (
-	"strings"
 	"testing"
-
-	"github.com/google/syzkaller/prog"
-	_ "github.com/google/syzkaller/sys"
-	"github.com/google/syzkaller/sys/targets"
 )
-
-func csbSource(t *testing.T, text string, threaded bool) string {
-	t.Helper()
-	target, err := prog.GetTarget(targets.Linux, targets.AMD64)
-	if err != nil {
-		t.Fatal(err)
-	}
-	p, err := target.Deserialize([]byte(text), prog.NonStrict)
-	if err != nil {
-		t.Fatal(err)
-	}
-	src, _, err := Write(p, Options{CSB: true, Threaded: threaded, Slowdown: 1})
-	if err != nil {
-		t.Fatal(err)
-	}
-	return string(src)
-}
-
-func requireCSource(t *testing.T, src, want string) {
-	t.Helper()
-	if !strings.Contains(src, want) {
-		t.Fatalf("generated C source does not contain %q", want)
-	}
-}
 
 func TestCSBTracksRegisteredRawIoUring(t *testing.T) {
 	src := csbSource(t, "r0 = io_uring_setup(0x1, &(0x7f0000000000))\n"+
